264bdaec2880aaa2f55a882c5384550d79a0f885
pjarrell <philip.jarrell@disney.com>
2018-05-12 11:17:48 -0400

    first commit

---
+++ src/main/java/com/visualpurity/parties/PartiesApplication.java
@@ -0,0 +1,12 @@
+package com.visualpurity.parties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartiesApplication.class, args);
+	}
+}
