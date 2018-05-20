8e094ce5c4b400ffce9d92eae3fccadcab5c6c2e
Philip Jarrell <philip@visualpurity.com>
2018-05-19 22:16:29 -0400

    Remove api key

--- src/main/frontend/src/store.js
+++ src/main/frontend/src/store.js
@@ -3,7 +3,7 @@ import 'firebase/firestore';
 import {initFirestorter, Collection} from 'firestorter';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAOzqkL9QXoJPSKQY4JdGKkxT1meBI3acw",
+    apiKey: "",
     authDomain: "parties-35d88.firebaseapp.com",
     databaseURL: "https://parties-35d88.firebaseio.com",
     projectId: "parties-35d88",
--- src/main/serverless/functions/index.js
+++ src/main/serverless/functions/index.js
@@ -8,6 +8,11 @@ admin.initializeApp();
 // from our app client-side.
 app.use(require('cors')({origin: true}));
 
+/**
+ * Authorizes a link for a specific attendee. If authorized, returns a token for login to Firebase.
+ * Links send to the attendees should go to
+ * https://us-central1-parties-35d88.cloudfunctions.net/api/attendee/authorize?token={email-token}.
+ */
 app.get('/attendee/authorize', (req, res) => {
     const token = req.query.token;
     admin
@@ -16,20 +21,30 @@ app.get('/attendee/authorize', (req, res) => {
         .where('token', '==', token)
         .get()
         .then((attendee) => {
-            return admin
-                .auth()
-                .createCustomToken(attendee.uid)
-                .then((customToken) => {
-                    return res.json({
-                        token: customToken
-                    });
+            if (attendee !== 'undefined' && attendee.uid && attendee.id !== '') {
+                console.log('Attendee found=' + JSON.stringify(attendee));
+                return res.json({
+                    token: token,
+                    attendee: attendee
                 });
+                /*return admin
+                    .auth()
+                    .createCustomToken(attendee.uid)
+                    .then((customToken) => {
+                        console.log('Firebase token created=' + customToken);
+                        return res.json({
+                            token: customToken
+                        });
+                    });*/
+            } else {
+                return Promise.reject(new Error('Failed to find the uid for attendee using token=' + token + ''));
+            }
         })
         .catch((error) => {
-            console.error('An invalid token was received', error);
+            console.error('An invalid token=' + token + ' was received', error);
             return res
                 .status(403)
-                .send('Unauthorized');
+                .send();
         });
 });
 
