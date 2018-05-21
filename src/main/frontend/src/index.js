0627e39acd77d438ceae05f2f229ddb4873bdccc
pjarrell <philip.jarrell@disney.com>
2018-05-20 23:00:15 -0400

    Email link auth

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
 import Index from './pages/index';
 import registerServiceWorker from './registerServiceWorker';
 
